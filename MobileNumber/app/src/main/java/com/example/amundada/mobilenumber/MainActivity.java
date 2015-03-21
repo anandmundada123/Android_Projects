6e8a149cd7b09202cd5a1a6be4373274063b4bd1
anandmundada123 <anandmundada123@gmail.com>
2015-03-20 21:07:51 -0700

    Get Mobile Number from device

---
+++ MobileNumber/app/src/main/java/com/example/amundada/mobilenumber/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.amundada.mobilenumber;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t = (TextView) findViewById(R.id.test);
+        TelephonyManager tMgr = (TelephonyManager)getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+
+        String mPhoneNumber = tMgr.getLine1Number();
+        //String text = mPhoneNumber + "," + tMgr.getCellLocation().toString();
+        t.setText(mPhoneNumber);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
